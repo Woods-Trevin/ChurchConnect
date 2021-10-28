a2ed6792d618a10ec1b3691daeeb27a23823e8c6
xanadel <trevinwoods41@gmail.com>
2021-10-27 19:47:58 -0500

    added about me information and a few links in footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,10 +1,21 @@
+import { NavLink } from 'react-router-dom'
 import './Footer.css';
 
 export default function Footer() {
 
     return (
-        <div>
-            <h1>Footer Component</h1>
+        <div className="Footer_ctnr">
+            <div className='aboutMe_container'>
+                <li className='ownerName' >Trevin Woods</li>
+                <span><a className='github' href='https://github.com/Woods-Trevin' target="_blank" rel="noreferrer">Github</a></span>
+                <span><a className='linkedIn' href='https://www.linkedin.com/in/trevinwoods4778661aa/' target="_blank" rel="noreferrer">LinkedIn</a></span>
+            </div>
+            <div className="quickNav_container">
+                <NavLink className="createEvent_footer_link" to="/event" > Create an Event </NavLink>
+                <NavLink className="createAnnouncement_footer_link" to="/announcement" > Create an Announcement </NavLink>
+                <NavLink className="home_footer_link" to="/" > Home </NavLink>
+
+            </div>
         </div>
     )
 }
