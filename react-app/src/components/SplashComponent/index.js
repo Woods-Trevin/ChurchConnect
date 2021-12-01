b406a9cceb8bb8bd55bc821ad412abf895f3cb78
xanadel <trevinwoods41@gmail.com>
2021-11-30 22:45:05 -0600

    added auto scrolling nasdaq kind of animation to the new prayer requests feature.

--- react-app/src/components/SplashComponent/index.js
+++ react-app/src/components/SplashComponent/index.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 import "./SplashComponent.css";
 
 export default function SplashComponent() {
+
+    const history = useHistory();
 
     const [splash_to_animate, set_splash_to_animate] = useState(1)
     // let splash_to_animate = 2
@@ -27,7 +30,7 @@ export default function SplashComponent() {
             <div className={`splashImg_Two ${splash_to_animate === 2 && "splash--visible"} ${splash_to_animate != 2 && "splash--hidden"}`} />
             <div className={`splashImg_One ${splash_to_animate === 1 ? "splash--visible" : "splash--hidden"}`} />
             <div className={`signupNewUser_btn_ctnr`} >
-                <li className={`signupNewUser_btn`} >Start Your Journey</li>
+                <li className={`signupNewUser_btn`} onClick={() => history.push("/sign-up")}>Start Your Journey</li>
             </div>
         </div>
     )
