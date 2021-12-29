c50bb8fa24ec0b6a1e9f50ebc8eeab11b12fe5f6
xanadel <trevinwoods41@gmail.com>
2021-12-28 21:25:56 -0600

    fixed bug where creation doesnt work because list that should have been returned was an invalid hashable list.

--- app/api/event_routes.py
+++ app/api/event_routes.py
@@ -108,7 +108,7 @@ def post_events():
             db.session.add(created_event)
             db.session.commit()
             events = Event.query.all()
-            return {events: [event.to_dict() for event in events]}
+            return {'events': [event.to_dict() for event in events]}
         else:
             return jsonify('Bad Data')
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
